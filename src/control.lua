5051f8f29d6ccec8ee9a6bf96bc4be420e9b0f2d
raiguard <caleb.heuer@gmail.com>
2022-03-20 21:53:34 -0600

    Remove debug seed

--- src/control.lua
+++ src/control.lua
@@ -3,8 +3,7 @@ local event = require("__flib__.event")
 local eller = require("scripts.eller")
 
 event.on_init(function()
-  -- TODO: Get rid of the 0
-  global.random = game.create_random_generator(0)
+  global.random = game.create_random_generator()
   local Row = eller.new(21)
   for _ = 1, 21 do
     Row = eller.step(Row, true)
