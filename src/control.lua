420d1145009de46b5d25acb76638ed4f184cdc11
raiguard <caleb.heuer@gmail.com>
2022-03-20 22:56:40 -0600

    Generate maze in-world

--- src/control.lua
+++ src/control.lua
@@ -1,11 +1,17 @@
 local event = require("__flib__.event")
 
-local eller = require("scripts.eller")
+local maze = require("scripts.maze")
 
 event.on_init(function()
-  global.random = game.create_random_generator()
-  local Row = eller.new(21)
-  for _ = 1, 21 do
-    Row = eller.step(Row, true)
+  -- Gatekeeping: Don't let people add this to an existing world
+  if game.tick ~= 0 then
+    error("Cannot add ribbon maze to an existing game")
   end
+
+  -- Guarantee the same maze on a given map seed and parameters
+  global.random = game.create_random_generator()
+
+  maze.init(32, 21)
 end)
+
+event.on_chunk_generated(maze.on_chunk_generated)
---
+++ src/scripts/maze.lua
@@ -0,0 +1,63 @@
+local area = require("__flib__.area")
+
+local eller = require("scripts.eller")
+
+local maze = {}
+
+--- @param box BoundingBox
+--- @param surface LuaSurface
+local function void_area(box, surface)
+  local tiles_to_set = {}
+  for pos in area.iterate(box) do
+    table.insert(tiles_to_set, { name = "out-of-map", position = { pos.x, pos.y } })
+  end
+  surface.set_tiles(tiles_to_set, true, true, true, true)
+end
+
+--- @param tile_size number
+--- @param width number
+function maze.init(tile_size, width)
+  -- Create global data
+  global.maze = {
+    Row = eller.new(math.ceil(width / 2)), -- The maze generator needs a halved width
+    rows = {},
+    tile_size = tile_size,
+    width = width,
+    x_boundary = math.floor(width / 2),
+    y = -1,
+  }
+end
+
+--- @param e on_chunk_generated
+function maze.on_chunk_generated(e)
+  --- @type ChunkPosition
+  local pos = e.position
+
+  -- If the chunk is outside the radius we care about, just remove it
+  local x_boundary = global.maze.x_boundary
+  if pos.x < -x_boundary or pos.x > x_boundary or pos.y < -1 then
+    void_area(e.area, e.surface)
+    return
+  end
+
+  -- Retrieve or generate row
+  local row = global.maze.rows[pos.y]
+  if not row then
+    for y = global.maze.y, pos.y, 2 do
+      -- TODO: Don't print maze rows
+      local NextRow, rows = eller.step(global.maze.Row, true)
+      global.maze.Row = NextRow
+      global.maze.rows[y] = rows[1]
+      global.maze.rows[y + 1] = rows[2]
+      global.maze.y = math.max(y + 2, global.maze.y)
+    end
+    row = global.maze.rows[pos.y]
+  end
+
+  local cell = pos.x + x_boundary + 1
+  if not row[cell] then
+    void_area(e.area, e.surface)
+  end
+end
+
+return maze
