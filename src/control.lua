5e8899509329d627eec9f9b97b659465c6952e11
raiguard <caleb.heuer@gmail.com>
2022-03-22 01:05:22 -0600

    Only create nauvis maze if starting a world

--- src/control.lua
+++ src/control.lua
@@ -5,23 +5,21 @@ local maze = require("scripts.maze")
 DEBUG = true
 
 event.on_init(function()
-  -- Gatekeeping: Don't let people add this to an existing world
-  if game.tick ~= 0 then
-    error("Cannot add ribbon maze to an existing game")
-  end
-
   -- Init mazes
   maze.init()
 
-  -- Create Nauvis maze
-  maze.new(
-    game.surfaces.nauvis,
-    settings.global["rrm-cell-size"].value,
-    settings.global["rrm-maze-width"].value,
-    settings.global["rrm-maze-height"].value,
-    -- Give a constant maze seed if in debug mode
-    DEBUG and 0 or nil
-  )
+  -- Only create a maze if we just started a world
+  if game.tick == 0 then
+    -- Create Nauvis maze
+    maze.new(
+      game.surfaces.nauvis,
+      settings.global["rrm-cell-size"].value,
+      settings.global["rrm-maze-width"].value,
+      settings.global["rrm-maze-height"].value,
+      -- Give a constant maze seed if in debug mode
+      DEBUG and 0 or nil
+    )
+  end
 end)
 
 event.on_surface_created(function(e)
